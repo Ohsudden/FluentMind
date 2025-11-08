ab76703b17d3f16c9fd5e9eae7fa25e34e8c0033
Overstar <mrganja122005@gmail.com>
2025-11-08 13:32:40 +0200

    Reformating structure of the project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
