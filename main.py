f40d7a30377129b09b8acd1da048b0df078d27e1
Overstar <mrganja122005@gmail.com>
2025-11-08 13:39:17 +0200

    Backend fix redirects

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ templates = Jinja2Templates(directory="static")
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/pricing", response_class=HTMLResponse)
+async def pricing(request: Request):
+    return templates.TemplateResponse("pricing.html", {"request": request})
+
+@app.get("/contacts", response_class=HTMLResponse)
+async def contacts(request: Request):
+    return templates.TemplateResponse("contacts.html", {"request": request})
+
+@app.get("/registration", response_class=HTMLResponse)
+async def registration(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
